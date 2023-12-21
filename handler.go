3dfb3e0abfff87726c283c4081edb5334853332e
HeroBuxx <me@herobuxx.me>
2023-12-21 23:00:35 +0700

    webhook: Randomize invalid secret code message
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- handler.go
+++ handler.go
@@ -1,6 +1,10 @@
+Copy code
 package webhook
 
 import (
+	"math/rand"
+	"time"
+
 	"github.com/aiteung/atmessage"
 	"github.com/aiteung/module/model"
 	"github.com/gofiber/fiber/v2"
@@ -20,13 +24,37 @@ func PostMessage(c *fiber.Ctx) error {
 		if err != nil {
 			return err
 		}
-		if ws.IsLoginRequest(msg, WAKeyword) { //untuk whatsauth request login
+
+		// Handle login request
+		if ws.IsLoginRequest(msg, WAKeyword) {
 			resp = HandlerQRLogin(msg, WAKeyword)
-		} else { //untuk membalas pesan masuk
+		} else {
 			resp = HandlerIncomingMessage(msg)
 		}
 	} else {
-		resp.Response = "Secret Salah"
+		// Random response for incorrect secret code
+		resp.Response = getRandomIncorrectSecretMessage()
 	}
 	return c.JSON(resp)
+}
+
+// getRandomIncorrectSecretMessage returns a random message for incorrect secret code
+func getRandomIncorrectSecretMessage() string {
+	// Seed the random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	// Array of possible incorrect secret messages
+	incorrectSecretMessages := []string{
+		"Oops! It seems like the secret code is incorrect.",
+		"Sorry, but the provided secret code is not valid.",
+		"Hmm, something went wrong. Please check the secret code and try again.",
+		"Error: Incorrect secret code. Please verify and resend your message.",
+		"Ups! kode loginnya salah nihh.",
+		"Maaf kak, kode kodenya ga valid :(",
+		"Hmm, coba cek kode loginnya lagi deh...",
+		"BANGG UDAH BANGG, KODENYA SALAHHH",
+	}
+
+	// Randomly select a message
+	return incorrectSecretMessages[rand.Intn(len(incorrectSecretMessages))]
 }
