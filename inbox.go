0a7fe453ba7dcd7f4425a9b39fd98e481c7e842e
HeroBuxx <me@herobuxx.me>
2023-12-21 22:05:13 +0700

    webhook: Add more message string and randomize them
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- inbox.go
+++ inbox.go
@@ -1,6 +1,10 @@
 package webhook
 
 import (
+	"fmt"
+	"math/rand"
+	"time"
+
 	"github.com/aiteung/atapi"
 	"github.com/aiteung/atmessage"
 	"github.com/aiteung/module/model"
@@ -8,16 +12,43 @@ import (
 )
 
 func HandlerIncomingMessage(msg model.IteungMessage) (resp atmessage.Response) {
+	// Seed the random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	// Array of message templates
+	messageTemplates := []string{
+		"Hai hai hai kak %s",
+		"Hello %s, how are you?",
+		"Hey there, %s! What's up?",
+		"Greetings, %s!",
+		"Hi %s, nice to hear from you!",
+		"Halo ka %s, lagi nyari apa nihh?",
+		// Add more templates as needed
+	}
+
+	// Randomly select a message template
+	selectedTemplate := messageTemplates[rand.Intn(len(messageTemplates))]
+
+	// Create the message with the selected template
+	message := fmt.Sprintf(selectedTemplate, msg.Alias_name)
+
+	// Create a wa.TextMessage
 	dt := &wa.TextMessage{
 		To:       msg.Chat_number,
 		IsGroup:  false,
-		Messages: "Hai hai hai kak " + msg.Alias_name,
+		Messages: message,
 	}
-	if msg.Chat_server == "g.us" { //jika pesan datang dari group maka balas ke group
+
+	// Check if the message is from a group
+	if msg.Chat_server == "g.us" {
 		dt.IsGroup = true
 	}
-	if (msg.Phone_number != "628112000279") && (msg.Phone_number != "6283131895000") { //ignore pesan datang dari iteung
+
+	// Ignore messages from specific phone numbers
+	if (msg.Phone_number != "628112000279") && (msg.Phone_number != "6283131895000") {
+		// Post the message using atapi.PostStructWithToken
 		resp, _ = atapi.PostStructWithToken[atmessage.Response]("Token", WAAPIToken, dt, "https://api.wa.my.id/api/send/message/text")
 	}
-	return
+
+	return resp
 }
